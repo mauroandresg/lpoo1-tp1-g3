98e271505d497fff3c55811cf29ac2004fd792c2
FranckLuna <franco7luna@gmail.com>
2024-05-01 21:48:14 -0300

    Clase Categoria

--- LPOOI_GRUPO_3/ClasesBase/Categoria.cs
+++ LPOOI_GRUPO_3/ClasesBase/Categoria.cs
@@ -7,12 +7,27 @@ namespace ClasesBase
 {
     public class Categoria
     {
-        private int cat_id;
+        private string cat_id;
 
-        public int Cat_id
+        public string Cat_id
         {
             get { return cat_id; }
             set { cat_id = value; }
+        }
+        private string cat_nombre;
+
+        public string Cat_nombre
+        {
+            get { return cat_nombre; }
+            set { cat_nombre = value; }
+        }
+
+        private string cat_descripcion;
+
+        public string Cat_descripcion
+        {
+            get { return cat_descripcion; }
+            set { cat_descripcion = value; }
         }
     }
 }
