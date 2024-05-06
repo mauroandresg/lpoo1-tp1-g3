a641ec121f8cd8fc33639796497b034aeb226869
Martinmgf95 <168382208+Martinmgf95@users.noreply.github.com>
2024-05-06 10:18:18 -0300

    se agrega el formulario de login y usuarios con roles para ingresar al sistema
    se agrega formulario de login para 3 usuarios con roles definidos en el formulario de login al hacer clic en el boton ingresar, tambien se agregaron los contructores para las clase de usuario y roles y se elimino Form1 y class1

---
+++ LPOOI_GRUPO_3/Vistas/FrmLogin.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Vistas
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIngresar_Click = new System.Windows.Forms.Button();
+            this.txtUsuario = new System.Windows.Forms.Label();
+            this.txtContraseña = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnIngresar_Click
+            // 
+            this.btnIngresar_Click.BackColor = System.Drawing.Color.Honeydew;
+            this.btnIngresar_Click.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIngresar_Click.Location = new System.Drawing.Point(312, 290);
+            this.btnIngresar_Click.Name = "btnIngresar_Click";
+            this.btnIngresar_Click.Size = new System.Drawing.Size(126, 35);
+            this.btnIngresar_Click.TabIndex = 0;
+            this.btnIngresar_Click.Text = "Ingresar";
+            this.btnIngresar_Click.UseVisualStyleBackColor = false;
+            this.btnIngresar_Click.Click += new System.EventHandler(this.btnIngresar_Click_Click);
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.AutoSize = true;
+            this.txtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(109, 152);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(79, 25);
+            this.txtUsuario.TabIndex = 1;
+            this.txtUsuario.Text = "Usuario";
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.AutoSize = true;
+            this.txtContraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContraseña.Location = new System.Drawing.Point(107, 228);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(114, 25);
+            this.txtContraseña.TabIndex = 2;
+            this.txtContraseña.Text = "Contraseña";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserName.Location = new System.Drawing.Point(284, 147);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(192, 30);
+            this.txtUserName.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(284, 223);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(192, 30);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("HYPMokGak-Bold", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.Color.BlueViolet;
+            this.label1.Location = new System.Drawing.Point(146, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(330, 44);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "BIENVENIDO";
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkKhaki;
+            this.ClientSize = new System.Drawing.Size(636, 392);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.btnIngresar_Click);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Iniciar Sesion";
+            this.Load += new System.EventHandler(this.FrmLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIngresar_Click;
+        private System.Windows.Forms.Label txtUsuario;
+        private System.Windows.Forms.Label txtContraseña;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ LPOOI_GRUPO_3/Vistas/FrmLogin.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ClasesBase;
+
+namespace Vistas
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIngresar_Click_Click(object sender, EventArgs e)
+        {
+
+            Boolean bUserFound = false;
+
+            Roles oRol2 = new Roles(2, "Vendedor");
+            Roles oRol3 = new Roles(3, "Operador");
+
+            Usuario oUsuario1 = new Usuario(1, "MartinF", "123", "Martin Flores", 1);
+            Usuario oUsuario2 = new Usuario(2, "JuanC", "456", "Juan Cardozo", 2);
+            Usuario oUsuario3 = new Usuario(3, "CarlosG", "789", "Carlos Gerez", 3);
+
+            if (oUsuario1.Usu_NombreUsuario == txtUserName.Text && oUsuario1.Usu_Contraseña == txtPassword.Text)
+            {
+                bUserFound = true;
+            }
+            else if (oUsuario2.Usu_NombreUsuario == txtUserName.Text && oUsuario2.Usu_Contraseña == txtPassword.Text)
+            {
+                bUserFound = true;
+            }
+            else if (oUsuario3.Usu_NombreUsuario == txtUserName.Text && oUsuario3.Usu_Contraseña == txtPassword.Text)
+            {
+                bUserFound = true;
+            }
+
+
+            if (bUserFound)
+            {
+                MessageBox.Show("Bienvenido/a: " + txtUserName.Text);
+            }
+            else
+            {
+                MessageBox.Show("Los datos son incorrectos");
+            }
+        }
+    }
+}
