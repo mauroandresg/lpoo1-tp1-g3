4e5eeb0d5cecbd8e411ea8213ceb5d8ef884351b
Alex Vega <ax.onestar@gmail.com>
2024-05-17 17:07:54 -0300

    Se enlazó la navegación a las nuevas vistas
    Se modificó la navegación, para ir hacia las nuevas vistas a travéz del menú.
    Se quitó código dejando lugar para la futura implementación funcinalidades basadas en la persistencia de datos con BD.

--- LPOOI_GRUPO_3/Vistas/FrmInicio.cs
+++ LPOOI_GRUPO_3/Vistas/FrmInicio.cs
@@ -12,10 +12,6 @@ namespace Vistas
 {
     public partial class FrmInicio : Form
     {
-        Categoria cat;
-        Disciplina dis;
-        Atleta atl;
-
         public FrmInicio()
         {
             InitializeComponent();
@@ -33,99 +29,35 @@ namespace Vistas
 
         private void MItemCatCrear_Click(object sender, EventArgs e)
         {
-            Formulario frmFormulario = new Formulario();
-            frmFormulario.TituloTxt = "Nueva Categoria";
-            frmFormulario.TipoFormulario = TipoForm.CATEGORIA;
-            frmFormulario.ShowDialog();
-
-            if (frmFormulario.NuevaEntidadCreada == true)
-            {
-                cat = new Categoria();
-                cat = frmFormulario.NuevaCat;
-            }
+            FrmAltaCategoria frmCategoria = new FrmAltaCategoria();
+            frmCategoria.Show();
         }
 
         private void MItemDisCrear_Click(object sender, EventArgs e)
         {
-            Formulario frmFormulario = new Formulario();
-            frmFormulario.TituloTxt = "Nueva Disciplina";
-            frmFormulario.TipoFormulario = TipoForm.DISCIPLINA;
-            frmFormulario.ShowDialog();
-
-            if (frmFormulario.NuevaEntidadCreada == true)
-            {
-                dis = new Disciplina();
-                dis = frmFormulario.NuevaDis;
-            }
+            FrmAltaDisciplina frmDisciplina = new FrmAltaDisciplina();
+            frmDisciplina.Show();
         }
 
         private void MItemPartCrear_Click(object sender, EventArgs e)
         {
-            Formulario frmFormulario = new Formulario();
-            frmFormulario.TipoFormulario = TipoForm.ATLETA;
-            frmFormulario.TituloTxt = "Nuevo Atleta";
-            frmFormulario.ShowDialog();
-
-            if (frmFormulario.NuevaEntidadCreada == true)
-            {
-                atl = new Atleta();
-                atl = frmFormulario.NuevoAtl;
-            }
+            FrmAltaParticipante frmParticipante = new FrmAltaParticipante();
+            frmParticipante.Show();
         }
 
         private void MItemCatMostrar_Click(object sender, EventArgs e)
         {
-            if (cat != null)
-            {
-                StringBuilder mensaje = new StringBuilder();
-                mensaje.AppendLine("Categoría" + Environment.NewLine);
-                mensaje.AppendLine("Nombre: " + cat.Cat_Nombre);
-                mensaje.AppendLine("Descripción: " + cat.Cat_Descripcion);
-                MessageBox.Show(mensaje.ToString(), "Nueva Categoría");
-            } else
-	        {
-                MensajeMostrarVacio();
-	        }
+            // Implementar usando la BD
         }
 
         private void MItemDisMostrar_Click(object sender, EventArgs e)
         {
-            if (dis != null)
-            {
-                StringBuilder mensaje = new StringBuilder();
-                mensaje.AppendLine("Disciplina" + Environment.NewLine);
-                mensaje.AppendLine("Nombre: " + dis.Dis_Nombre);
-                mensaje.AppendLine("Descripción: " + dis.Dis_Descripcion);
-                MessageBox.Show(mensaje.ToString(), "Nueva Disciplina"); 
-            } else
-	        {
-                MensajeMostrarVacio();
-	        }
+            // Implementar usando la BD
         }
 
         private void MItemPartMostrar_Click(object sender, EventArgs e)
         {
-            if (atl != null)
-            {
-                StringBuilder mensaje = new StringBuilder();
-                mensaje.AppendLine("Atleta" + Environment.NewLine);
-                mensaje.AppendLine("DNI: " + atl.Atl_DNI + " - Genero: " + atl.Atl_Genero);
-                mensaje.AppendLine("Nombre y Apellido: " + atl.Atl_Nombre + " " + atl.Atl_Apellido);
-                mensaje.AppendLine("Nacionalidad: " + atl.Atl_Nacionalidad);
-                mensaje.AppendLine("Entrenador: " + atl.Atl_Entrenador);
-                mensaje.AppendLine("Altura: " + atl.Atl_Altura + "Mts - Peso:" + atl.Atl_Peso + "KG");
-                mensaje.AppendLine("Fecha de nacimiento: " + atl.Atl_FechaNac.ToShortDateString());
-                mensaje.AppendLine("Dirección/Domicilio: " + atl.Atl_Direccion);
-                mensaje.AppendLine("Email de contacto: " + atl.Atl_Email);
-                MessageBox.Show(mensaje.ToString(), "Nuevo participante"); 
-            } else
-	        {
-                MensajeMostrarVacio();
-	        }
-        }
-
-        private void MensajeMostrarVacio() {
-            MessageBox.Show("Aún no se cargaron datos...", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            // Implementar usando la BD
         }
     }
 }
