bacfc5b50c42dd11acda551ac28dbc0abaea84b5
mauroandresg <maurocraxker@gmail.com>
2024-05-20 14:30:00 -0300

    Hago la carga de la base para la conexion de datos

---
+++ LPOOI_GRUPO_3/ClasesBase/TrabajarUsuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClasesBase
+{
+    class TrabajarUsuario
+    {
+        public static DataTable list_roles(){
+            SqlConecttion cnn =  new SqlConecttion(ClasesBase.Properties.Settings.Default.comdepConnectionString);
+
+    }
+}
