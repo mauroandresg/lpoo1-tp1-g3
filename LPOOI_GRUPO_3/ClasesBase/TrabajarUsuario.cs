6afa6c92c65a8751943195ed34741d928ac8644c
mauroandresg <maurocraxker@gmail.com>
2024-05-20 14:38:57 -0300

    Modifico el trabajar usuario y agrego la conexion y carga de base de datos

--- LPOOI_GRUPO_3/ClasesBase/TrabajarUsuario.cs
+++ LPOOI_GRUPO_3/ClasesBase/TrabajarUsuario.cs
@@ -3,12 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.Data;
+using System.Data.SqlClient;
+
 namespace ClasesBase
 {
     class TrabajarUsuario
     {
-        public static DataTable list_roles(){
-            SqlConecttion cnn =  new SqlConecttion(ClasesBase.Properties.Settings.Default.comdepConnectionString);
+        public static DataTable list_roles()
+        {
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.comdepConnectionString);
+            
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "SELECT * FROM Roles";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cnn;
 
+            //Ejecuta la consulta sql
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+            //Llena los datos de la tabla
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            return dt;
+        }
     }
 }
