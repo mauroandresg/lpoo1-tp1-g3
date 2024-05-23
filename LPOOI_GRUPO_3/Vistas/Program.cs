a7727a0426739b3575653a44d0dab2019b339e8e
Martinmgf95 <168382208+Martinmgf95@users.noreply.github.com>
2024-05-23 11:12:41 -0300

    agrego espacio
    se agrega espacio

--- LPOOI_GRUPO_3/Vistas/Program.cs
+++ LPOOI_GRUPO_3/Vistas/Program.cs
@@ -13,6 +13,7 @@ namespace Vistas
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmLogin());
